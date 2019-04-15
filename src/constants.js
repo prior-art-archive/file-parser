34a5738c39f0299bcd235b343128feb39a56b155
Joel Gustafson <joelg@mit.edu>
2019-04-15 10:53:54 -0400

    changed constant TikaUrl to environment variable TIKA_URL

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,18 @@
-const { NODE_ENV } = process.env
+const { NODE_ENV, TIKA_URL } = process.env
 const subdomain = NODE_ENV === "development" ? "dev" : "www"
 
 const DocumentIdKey = "document-id"
 const OriginalFilenameKey = "original-filename"
 
 // This container is "linked" to the Tika container
-const TikaUrl = "http://tika:9998"
 
 const MetaRequest = {
-	url: `${TikaUrl}/meta/form`,
+	url: `${TIKA_URL}/meta/form`,
 	headers: { Accept: "text/csv" },
 }
 
 const TextRequest = {
-	url: `${TikaUrl}/tika/form`,
+	url: `${TIKA_URL}/tika/form`,
 	headers: { Accept: "text/plain" },
 }
 
